6fb15a9c33854c09a0ffeaa36b394f4977db98ed
Sergiorioss <apple@apples-MacBook.local>
2024-06-25 00:51:28 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { ImageAccordion } from './components/ImageAccordion/ImageAccordion'
+
+function App() {
+    return (
+        <>
+            <ImageAccordion />
+        </>
+    )
+}
+
+export default App
