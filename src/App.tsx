6d2c6f702a2b0fda6277304f84c0188984bbc0fd
Sergiorioss <apple@apples-MacBook.local>
2024-06-25 23:50:00 -0600

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import Sidebar from './components/Sidebar/Sidebar'
 function App() {
     return (
         <div className='container'>
-            <h1>myMusic</h1>
+            <div className="row">
+                <div className="col">
+                    <h1 className='mb-5 text-secondary'>myMusic</h1>
+                </div>
+            </div>
             <div className="row">
                 <div className="col-2">
                     <Sidebar />
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,42 +1,56 @@
+import { useState } from 'react'
 import './styles.css'
 
 const Sidebar = () => {
+    const [loggIn, setLoggin] = useState<boolean>(false)
     return (
         <aside className="sidebar">
             <div className="header">
-                <img src="https://w7.pngwing.com/pngs/543/1020/png-transparent-spotify-computer-icons-music-jenoti-logo-symbol-spotify-logo.png" />
-                <h1>Violet AI</h1>
+                {
+                    loggIn ? <><img className="imgHeader" src="./src/assets/spotify.png" /><h1 className='m-2'>Hi ! Firstname</h1></>
+                        : <><img className="imgHeader" src="./src/assets/spotify.png" />
+                            <h1>Welcome to myMusic</h1></>
+                }
             </div>
-            <nav>
-                <a>
-                    <i className="ai-search"></i>
-                    <p>Search</p>
-                </a>
-                <a>
-                    <i className="ai-home"></i>
-                    <p>Home</p>
-                </a>
-                <a>
-                    <i className="ai-folder"></i>
-                    <p>Projects</p>
-                </a>
-                <a>
-                    <i className="ai-dashboard"></i>
-                    <p>Dashboard</p>
-                </a>
-                <a>
-                    <i className="ai-person"></i>
-                    <p>Team</p>
-                </a>
-                <a>
-                    <i className="ai-envelope"></i>
-                    <p>Support</p>
-                </a>
-                <a>
-                    <i className="ai-gear"></i>
-                    <p>Settings</p>
-                </a>
-            </nav>
+            {
+                loggIn ? <nav>
+                    <a>
+                        <i className="ai-search"></i>
+                        <p>Search</p>
+                    </a>
+                    <a>
+                        <i className="ai-heart"></i>
+                        <p>Favorite</p>
+                    </a>
+                    <a>
+                        <i className="ai-envelope"></i>
+                        <p>Support</p>
+                    </a>
+                    <a>
+                        <i className="ai-gear"></i>
+                        <p>Logout</p>
+                    </a>
+                </nav> :
+                    <nav>
+                        <a>
+                            <i className="ai-person"></i>
+                            <p>Log in</p>
+                        </a>
+                        <a>
+                            <i className="ai-save"></i>
+                            <p>Register</p>
+                        </a>
+                        <a>
+                            <i className="ai-search"></i>
+                            <p>Search</p>
+                        </a>
+                        <a>
+                            <i className="ai-envelope"></i>
+                            <p>Support</p>
+                        </a>
+                    </nav>
+            }
+
         </aside>
     )
 }
