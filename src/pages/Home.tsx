c9607f131f9b209143d3ca74045be10b29e6d17e
Sergiorioss <apple@apples-MacBook.local>
2024-06-28 00:22:28 -0600

    integrate spinner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react'
 import { ImageAccordion } from '../components/ImageAccordion/ImageAccordion'
 import Layout from '../components/Layout/Layout'
+import { ScaleLoader } from 'react-spinners'
 
 const Home = () => {
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 4000)
+    }, [])
+
     return (
         <Layout>
-            <ImageAccordion />
+            {
+                loading ? <ScaleLoader /> : <ImageAccordion />
+            }
         </Layout>
     )
 }
